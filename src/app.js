48ae8027d5d8a7728d09c2917457778edfa44f88
Georgi254 <georgi.gyovchev254@gmail.com>
2023-01-11 21:07:32 +0000

    Upload

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['My dog', 'My cat', 'My turtle', 'A police officer', 'A clown', 'My grandma'];
+  let what = ['ate', 'broke', 'stole', 'ripped','destroyed'];
+  let when = ['my homework', 'my newspaper', 'my will to live', 'my sandwich', 'my nephew'];
+  let emoji = ['<i class="bi bi-person-slash"></i>','<i class="bi bi-emoji-smile-upside-down"></i>', '<i class="bi bi-emoji-dizzy"></i>', '<i class="bi bi-emoji-frown"></i>', '<i class="bi bi-emoji-neutral"></i>', '<i class="bi bi-emoji-sunglasses-fill"></i>']
+  
+
+  let whoIndex = [Math.floor(Math.random() * who.length)]
+  let whatIndex = [Math.floor(Math.random() * what.length)]
+  let whenIndex = [Math.floor(Math.random() * when.length)]
+  let emojiIndex = [Math.floor(Math.random() * emoji.length)]
+
+
+  let excuse = who[whoIndex] + ' ' + what[whatIndex] + ' ' + when[whenIndex] + ' ' + emoji[emojiIndex];
+  let element = document.querySelector('#excuse')
+  let divimage = document.querySelector('#image')
+  element.innerHTML = excuse;
+  
+
+  console.log(excuse)
 };
+
